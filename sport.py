de30c7cea971ebac7513d344c361578b195b5b0e
Tibo Mousset <83610454+Tibo-Mouss@users.noreply.github.com>
2022-06-24 16:00:25 +0200

    Ajout du bouton previous, d'un timer

--- sport.py
+++ sport.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from random import randint
 import string
 import time
@@ -16,25 +17,16 @@ class Sport_Coach_Generator():
     liste_affichage = []
 
     def __init__(self):
+        self.init_series()
 
+    # Initialise des séries d'exercices à faire
+    def init_series(self):
         with open('D:/Documents/Sport/sportcsv.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
                 #print (row)
                 self.liste += [row]
-
-        f = open("D:/Documents/Sport/output.txt", "w")
-
-
-        f.write("ECHAUFFEMENT \n \n")
-        f.write("Jumping Jacks : 50 \n")
-        f.write("Levers de genous : 50 \n")
-        f.write("Crunchies : 20 \n")
-        f.write("  \n")
-        f.write("------------------------------------- \n")
-        f.write("LA CA VA CHIER \n")
-        f.write("------------------------------------- \n")
 
         self.liste_affichage.append(["ECHAUFFEMENT \n \n","Jumping Jacks : 50 \n","Levers de genous : 50 \n","Crunchies : 20 \n","------------------------------------- \n","LA CA VA CHIER \n"])
 
@@ -50,26 +42,42 @@ class Sport_Coach_Generator():
                 points_a_enlever = iterations_activite * int(x[1])
             self.points -= points_a_enlever
             texte = x[0] + " : " + str(iterations_activite) + " \n"
-            f.write(texte)
             bloc_text.append(texte)
 
             compteur_pour_faire_une_pause += points_a_enlever
             if compteur_pour_faire_une_pause > self.pts_pour_pause:
                 bloc_text.append("Pause d'une minute ! \n  \n")
-                f.write("Pause d'une minute ! \n  \n")
 
                 self.liste_affichage.append(bloc_text)
                 bloc_text = []
                 compteur_pour_faire_une_pause = 0
 
+        self.liste_affichage.append("  \n ------------------------------------- \n FIN, place aux étirements ! \n")
+    
+    def output_txt(self):
+        f = open("D:/Documents/Sport/output.txt", "w")
+
+        f.write("ECHAUFFEMENT \n \n")
+        f.write("Jumping Jacks : 50 \n")
+        f.write("Levers de genous : 50 \n")
+        f.write("Crunchies : 20 \n")
+        f.write("  \n")
+        f.write("------------------------------------- \n")
+        f.write("LA CA VA CHIER \n")
+        f.write("------------------------------------- \n")
+
+        f.write("Pause d'une minute ! \n  \n")
+
+        for bloc_serie in self.liste_affichage:
+            for i in bloc_serie:
+                f.write(i)
+        
         f.write("  \n")
         f.write("------------------------------------- \n")
         f.write("FIN, place aux étirements ! \n")
 
         f.close()
 
-        self.liste_affichage.append("  \n ------------------------------------- \n FIN, place aux étirements ! \n")
-
-        print("C'est fait chef !")
-
-    
+    def open_output_file_notepad(self):
+        osCommandString = "notepad.exe D:/Documents/Sport/output.txt"
+        os.system(osCommandString)
